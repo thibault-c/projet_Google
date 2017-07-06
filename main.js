f1f4000ceb50296a5e03d23078c40a1e0e19f194
Thibault <thibault.courrie@gmail.com>
2017-07-06 22:27:02 +0200

    version avec searchbar + main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+var monboutton = document.querySelector('#b1');
+
+monboutton.onclick = function funct() 
+
+{
+var  a = document.getElementById('middle');
+a.style.visibility = "visible";
+var  a = document.getElementById('b1');
+a.style.visibility = "hidden";
+}
+
+// get a value from the search bar
+
+var b2 = document.getElementById('#b2');
+
+b2.onclick = function get_searchbar()
+{
+var recherche = document.getElementById('search');
+recherche.value
+
+}
